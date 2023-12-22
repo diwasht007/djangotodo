6c981986f107d3be43386e6d75ef32d2ccba6e6e
diwash tiwari <diwashtiwari007@gmail.com>
2023-12-22 19:16:35 +1100

    completed till mark as done

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,9 +4,14 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_task = Task.objects.filter(is_completed=True).order_by('-updated_at')
     context={
-        'tasks':tasks
+        'tasks':tasks,
+        'completed_task':completed_task,
+        
     }
-    
     # return HttpResponse('<h1>This is my home page </h1>')
     return render(request,'home.html',context)
+
+
+    
