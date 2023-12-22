cd2da8e4c03366244e9ff7af5af2c7f039d025d7
diwash tiwari <diwashtiwari007@gmail.com>
2023-12-23 00:13:00 +1100

    todo app file

--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,21 @@ from .import views
 
 
 urlpatterns = [
+    #ADD TASK
     path('addTask/',views.addTask,name='addTask'),
-    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done')
+    
+    #mark as done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    
+    #mark as undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    
+    #edit features
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    
+    #delete feature
+    
+    path('delete_task/<int:pk>',views.delete_task,name='delete_task')
+    
 
 ]
